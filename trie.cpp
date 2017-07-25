0a23cab780a01bbdf87d15c17a852cf7e26a7e55
Duc-cmn-Le <ldcduc@apcs.vn>
2017-07-25 07:22:15 +0700

    Edit Insertion

--- trie.cpp
+++ trie.cpp
@@ -85,6 +85,27 @@ int Trie::Search(string &s) {
 }
 
 // Gia Bao
+
+void Trie::Extract_word(string &s)
+{
+    string tmp;
+    for(string::iterator i=s.begin();i!=s.end();i++)
+    if (*i!=' ' && *i != ':' && *i != '.' && *i != '?' && *i != ';' && *i != '!' && *i != ',' && *i != '\'')
+    {
+        tmp=tolower(*i);
+        while (i!=s.end()-1)
+        if (*(++i)!=' ' && *(i)!=':' &&  *(i)!='.' && *(i)!='?' && *(i)!=';' && *(i)!='!' && *i != ',')
+            tmp+=tolower(*i);
+        else break;
+        Insert(tmp);
+        if (*(tmp.end()-1)=='\'')
+        {
+            tmp.erase(tmp.end()-1);
+            Insert(tmp);
+        }
+    }
+}
+/*
 void Trie::Extract_word(string &s)
 {
     string tmp;
@@ -99,6 +120,7 @@ void Trie::Extract_word(string &s)
         Insert(tmp);
     }
 }
+*/
 
 // Gia Bao
 void Trie::Input_file(istream &fin) {
