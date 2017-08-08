f7c8ea51f2063b6df07013100c89b857f5911da6
Duc-cmn-Le <ldcduc@apcs.vn>
2017-08-08 22:00:10 +0700

    Coding query processing

--- function.cpp
+++ function.cpp
@@ -4,12 +4,14 @@ Trie::~Trie() {
 
 }
 
+// Cong Duc
 void Trie::Destruct(details *cur) {
     if (cur == NULL) return;
     Destruct(cur->next);
     delete cur;
 }
 
+// Cong Duc
 void Trie::Destruct(Node *cur) {
     if (cur == NULL) return;
     Destruct(cur->info);
@@ -18,6 +20,7 @@ void Trie::Destruct(Node *cur) {
     delete cur;
 }
 
+// Cong Duc
 void Trie::Insert(const char* s,int file_id,int pos) {
     Node *cur = root;
     for (int i=0;i<strlen(s);++i) {
@@ -44,16 +47,7 @@ void Trie::Insert(const char* s,int file_id,int pos) {
     }
 }
 
-Node* Trie::Find(char *s) {
-    Node *cur = root;
-    for (int i=0;i<strlen(s);++i) {
-        if (cur->next[s[i]] == NULL)
-            return NULL;
-        cur = cur->next[s[i]];
-    }
-    return cur;
-}
-
+// Cong Duc
 void Trie::Show_trie(Node *cur,string s) {
     if (cur->point > 0) {
         details *info_cur = cur->info;
@@ -67,6 +61,51 @@ void Trie::Show_trie(Node *cur,string s) {
         if (cur->next[i] != NULL) Show_trie(cur->next[i],s+(char)i);
 }
 
+// Cong Duc
+Node* Trie::Find(char *s) {
+    Node *cur = root;
+    for (int i=0;i<strlen(s);++i) {
+        if (cur->next[s[i]] == NULL)
+            return NULL;
+        cur = cur->next[s[i]];
+    }
+    return cur;
+}
+
+// Cong Duc
+int Query(int flag,Trie T,char *word,int *rating,int &number_of_file) {
+    // flag
+    // OR - 0
+    // AND - 1
+    // -united - -oo
+    // +and - 2
+    // 
+    Node *_find = T.Find(word);
+    if (flag == -oo && _find->point == 0) return true;
+    int *cnt = new int [number_of_file];
+    ZERO(cnt);
+    if (_find != NULL && _find.point > 0) {
+        details *info = _find->info;
+        while (info != NULL) {
+            cnt[info->file_id] += info->pos.size();
+            info = info->next;
+        }
+    }
+    if (flag == -oo) 
+        for (int i=0;i<number_of_file;++i)
+            if (cnt[i] > 0) rating[i] -= oo;
+    else if (flag == 0 || flag == 2) {
+        for (int i=0;i<number_of_file;++i)
+            if (rating[i] > 0) rating[i] += cnt[i];
+    }
+    else if (flag == 1) {
+        for (int i=0;i<number_of_file;++i)
+            if (cnt[i] == 0) rating = 0;
+            else if (rating[i] > 0) rating[i] += cnt[i];
+    }
+}
+
+// Cong Duc
 void Open_file(istream &fin,string &title,string &content) {
     content = "";
     string s;
@@ -77,11 +116,118 @@ void Open_file(istream &fin,string &title,string &content) {
     }
 }
 
+// Cong Duc
+void Trie::Destruct(Node *cur) {
+    if (cur == NULL) return;
+    Destruct(cur->info);
+    for (int i=0;i<256;++i)
+        Destruct(cur->next[i]);
+    delete cur;
+}
+
+// Cong Duc
+void Trie::Insert(const char* s,int file_id,int pos) {
+    Node *cur = root;
+    for (int i=0;i<strlen(s);++i) {
+        if (cur->next[s[i]] == NULL)
+            cur->next[s[i]] = new Node;
+        cur = cur->next[s[i]];
+    }
+    cur->point++;
+    if (cur->info == NULL) {
+        cur->info = new details;
+        cur->info->file_id = file_id;
+        cur->info->pos.push_back(pos);
+    }
+    else {
+        details *info_cur = cur->info, *info_prev;
+        while (info_cur != NULL && info_cur->file_id != file_id)
+            info_prev = info_cur, info_cur = info_cur->next;
+        if (info_cur == NULL) {
+            info_prev->next = new details;
+            info_cur = info_prev->next;   
+            info_cur->file_id = file_id;
+        }
+        info_cur->pos.push_back(pos);
+    }
+}
+
+// Cong Duc
+void Trie::Show_trie(Node *cur,string s) {
+    if (cur->point > 0) {
+        details *info_cur = cur->info;
+        while (info_cur != NULL) {
+            for (vector<int>::iterator i=info_cur->pos.begin();i!=info_cur->pos.end();++i)
+                cout << info_cur->file_id << ' ' << s << ' ' << *i << '\n';
+            info_cur = info_cur->next;
+        }
+    }
+    for (int i=0;i<256;++i)
+        if (cur->next[i] != NULL) Show_trie(cur->next[i],s+(char)i);
+}
+
+// Cong Duc
+int Trie::Find(char *s) {
+    Node *cur = root;
+    for (int i=0;i<strlen(s);++i) {
+        if (cur->next[s[i]] == NULL)
+            return NULL;
+        cur = cur->next[s[i]];
+    }
+    return cur->point > 0;
+}
+
+// Cong Duc
+Node* Trie::Find(char *s) {
+    Node *cur = root;
+    for (int i=0;i<strlen(s);++i) {
+        if (cur->next[s[i]] == NULL)
+            return NULL;
+        cur = cur->next[s[i]];
+    }
+    return cur;
+}
+
+// Cong Duc
+int Query(int flag,Trie T,char *word,int *rating,int &number_of_file) {
+    // flag
+    // OR - 0
+    // AND - 1
+    // -united - -oo
+    // +and - 2
+    // 
+    Node *_find = T.Find(word);
+    if (flag == -oo && _find->point == 0) return true;
+    int *cnt = new int [number_of_file];
+    ZERO(cnt);
+    if (_find != NULL && _find.point > 0) {
+        details *info = _find->info;
+        while (info != NULL) {
+            cnt[info->file_id] += info->pos.size();
+            info = info->next;
+        }
+    }
+    if (flag == -oo) 
+        for (int i=0;i<number_of_file;++i)
+            if (cnt[i] > 0) rating[i] -= oo;
+    else if (flag == 0 || flag == 2) {
+        for (int i=0;i<number_of_file;++i)
+            if (rating[i] > 0) rating[i] += cnt[i];
+    }
+    else if (flag == 1) {
+        for (int i=0;i<number_of_file;++i)
+            if (cnt[i] == 0) rating = 0;
+            else if (rating[i] > 0) rating[i] += cnt[i];
+    }
+}
+
+// Cong Duc
 void Free_file(char*& title, char*& content) {
     delete []title;
     delete []content;
 }
 
+// Cong Duc
 int FILTERING(string file_name) { 
     ifstream fin("CS163-Data/"+file_name);
     ofstream fout("tmpfileout.txt",ofstream::out);
@@ -101,6 +247,8 @@ int Check_all_space(string &s) {
     return true;
 }
 
+
+// Cong Duc
 int Next_token(string &s,string &target,char c) {
     if (s.length() == 0) return false;
     target = "";
@@ -115,4 +263,9 @@ int Next_token(string &s,string &target,char c) {
     target = s;
     s = "";
     return true;
+}
+
+// Cong Duc
+bool compare_by_rating(int i,int j) {
+    return rating[i] > rating[j];
 }
--- function.hpp
+++ function.hpp
@@ -1,5 +1,5 @@
 #pragma once
-#include <ctype.h>
+
 #include <cmath>
 #include <vector>
 #include <string>
@@ -7,12 +7,15 @@
 #include <iomanip>
 #include <fstream>
 #include <cstring>
+#include <ctype.h>
 #include <iostream>
 #include <algorithm>
 
+#define equal =
+#define oo 10000000
 #define DEBUG(x) cout << x
 #define DEBUN(x) cout << x << '\n'
-#define equal =
+#define ZERO(x) for (int i=0;i<number_of_file;++i) *(x+i) = 0;
 
 using namespace std;
 
@@ -23,18 +26,19 @@ struct details {
 };
 class Node {
 public:
-    Node() {for (int i=0;i<256;++i) next[i] = NULL;}
     Node *next[256];
     details *info = NULL; // info is a linkedlist
     int point = 0;
+    Node() {for (int i=0;i<256;++i) next[i] = NULL;}
 };
 
 struct Trie{
+    Node *root = new Node;
     ~Trie();
     void Destruct(details*);
     void Destruct(Node*);
-    Node *root = new Node;
     void Insert(const char*,int,int); // word, file_id, pos_in_content
+    int Find(char*);
     Node *Find(char*);
     void Show_trie(Node *cur,string s ="");
 
@@ -46,8 +50,10 @@ struct Data {
     char *content = NULL;
 };
 
+int Query(int flag,Trie T,char *word,int *rating,int &number_of_file) {
 void Open_file(istream &,string &,string &);
 void Free_file(char *&,char*&);
 int FILTERING(string file_name);
 int Check_all_space(string &s);
 int Next_token(string &s,string &target,char c=' ');
+bool compare_by_rating(int,int);
